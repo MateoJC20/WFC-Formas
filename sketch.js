c5bac06fa4ea26c56d793845da9a8fd589c0a139
Mateo Correa <jmateo.correa@ucuenca.edu.ec>
2022-12-21 10:56:51 -0500

    Ingreso y estructura de datos

--- sketch.js
+++ sketch.js
@@ -1,7 +1,79 @@
+const celdas = []; //celdas de 4x4
+const RETICULA = 4;
+
+const azulejos = [];
+const NA = 6; //número de azulejos
+
+const reglas = [
+  //Reglas de los bordes de cada azulejo
+  {
+    //Tiles0
+    UP:1,
+    RIGHT:0,
+    DOWN:1,
+    LEFT:0,
+  },
+  {
+    //Tiles1
+    UP:1,
+    RIGHT:1,
+    DOWN:0,
+    LEFT:1,
+  },
+  {
+    //Tiles2
+    UP:0,
+    RIGHT:1,
+    DOWN:1,
+    LEFT:1,
+  },
+  {
+    //Tiles3
+    UP:1,
+    RIGHT:0,
+    DOWN:1,
+    LEFT:1,
+  },
+  {
+    //Tiles4
+    UP:1,
+    RIGHT:1,
+    DOWN:1,
+    LEFT:0,
+  },
+  //Tiles5
+  {
+    UP:0,
+    RIGHT:0,
+    DOWN:1,
+    LEFT:1,
+  },
+
+];
+
+function preLoad(){
+  for(let i = 0; i < NA; i++ ){
+    azulejos[i] = loadImage(`azulejos/Tiles${i}.png`);
+  }
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1080, 1080);
+let opcionesI = [];
+for(let i = 0; i < azulejos.length; i++){
+  opcionesI.push(i);
+}
+
+for(let i = 0; i < RETICULA * RETICULA; i ++){
+  celdas[i] = {
+    colapsada: false,
+    opciones: opcionesI
+  }
+}
+print(celdas);
+
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+
 }
