ffd0d0a2bcbf640be4266e50c3c43dfb34bad29b
Mateo Correa <jmateo.correa@ucuenca.edu.ec>
2022-12-21 14:36:39 -0500

    continuación código WFC
    error en celdaSeleccionada

--- sketch.js
+++ sketch.js
@@ -4,6 +4,9 @@ const RETICULA = 4;
 const azulejos = [];
 const NA = 6; //número de azulejos
 
+let ancho;  //ancho de celdas
+let alto; //altura de celdas
+
 const reglas = [
   //Reglas de los bordes de cada azulejo
   {
@@ -41,8 +44,8 @@ const reglas = [
     DOWN:1,
     LEFT:0,
   },
-  //Tiles5
   {
+    //Tiles5
     UP:0,
     RIGHT:0,
     DOWN:1,
@@ -51,7 +54,7 @@ const reglas = [
 
 ];
 
-function preLoad(){
+function preload(){
   for(let i = 0; i < NA; i++ ){
     azulejos[i] = loadImage(`azulejos/Tiles${i}.png`);
   }
@@ -59,21 +62,67 @@ function preLoad(){
 
 function setup() {
   createCanvas(1080, 1080);
-let opcionesI = [];
-for(let i = 0; i < azulejos.length; i++){
-  opcionesI.push(i);
-}
 
-for(let i = 0; i < RETICULA * RETICULA; i ++){
-  celdas[i] = {
-    colapsada: false,
-    opciones: opcionesI
+  ancho = width / RETICULA;
+  alto = height / RETICULA;
+
+  let opcionesI = [];
+
+  for(let i = 0; i < azulejos.length; i++){
+    opcionesI.push(i);
   }
-}
-print(celdas);
 
+  for(let i = 0; i < RETICULA * RETICULA; i ++){
+    celdas[i] = {
+      colapsada: false,
+      opciones: opcionesI,
+    }
+  }
+
+  print(azulejos)
 }
 
 function draw() {
+  
+  const celdasDisponibles = celdas.filter((celda) =>{ 
+  return celda.colapsada == false
+  });
 
+  if(celdasDisponibles.length > 0){
+    celdasDisponibles.sort((a, b) =>{
+      return a.opciones.length - b.opciones.length;
+    });
+    print(celdasDisponibles)
+
+    const celdasPorColapsar = celdasDisponibles.filter((celda)=>{
+      return celda.opciones.length == celdasDisponibles[0].length;
+    });
+    
+    
+    const celdaSeleccionada = random (celdasPorColapsar);
+    celdaSeleccionada.colapsada = true;
+
+    const opcionSeleccionada = random(celdaSeleccionada.opciones);
+    celdaSeleccionada.opciones = [opcionSeleccionada];
+
+    
+    for(let x = 0; x < RETICULA; x++){
+      for(let y = 0; y < RETICULA; y++){
+        const celdaIndex = x + y * RETICULA;
+        const celdaActual = celdas[celdaIndex];
+        if(celdaActual.colapsada){
+          Image(
+            azulejos[celdaActual.opciones[0]], 
+            x * ancho, 
+            y * alto,
+            ancho,
+            alto
+            );
+        }
+      }
+    
+    }
+
+    noLoop();
+  }
 }
